fc09c636550442852303f5deb2171f243f1e750d
JohnWjck <neo_soti@hotmail.com>
2021-07-19 18:00:37 -0400

    v1

---
+++ Ejemplo/Assets/ControlJuego.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ControlJuego : MonoBehaviour {
+
+	static public int nivelesDesbloqueados;
+	public int nivelActual;
+	public Button[] botonesMenu;
+	CargarYGuardar cargarYGuardar;
+
+	void Awake(){
+		cargarYGuardar = GetComponent<CargarYGuardar> ();
+	}
+
+	void Start(){
+		if (SceneManager.GetActiveScene ().name == "Menu") {
+			cargarYGuardar.Guardar ();
+			ActualizarBotones ();
+		}
+	}
+
+
+	public void CambiarNivel(int nivel){
+		if (nivel == 0) {
+			SceneManager.LoadScene ("Menu");
+		} else {
+			SceneManager.LoadScene ("Nivel_" + nivel);
+		}
+	}
+
+	public void DesbloquearNivel(){
+		if (nivelesDesbloqueados < nivelActual) {
+			nivelesDesbloqueados = nivelActual;
+			nivelActual++;
+		}
+		VolverAlMenu ();
+
+	}
+
+	void VolverAlMenu(){
+		CambiarNivel (0);
+	}
+
+	public void ActualizarBotones(){
+		for (int i = 0; i < nivelesDesbloqueados+1; i++) {
+			botonesMenu [i].interactable = true;
+		}
+	}
+
+
+}
---
+++ Ejemplo/Assets/ControlPuntuacion.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlPuntuacion : MonoBehaviour {
+	public Text textoPuntuacion;
+	int puntuacion=0;
+	ControlJuego controlJuego;
+
+	void Awake(){
+		controlJuego = GameObject.Find ("ControlJuego").GetComponent (typeof(ControlJuego)) as ControlJuego;
+	}
+
+
+	public void SumarPunto(){
+		puntuacion++;
+		ActualizarTextoPuntuacion ();
+
+		if (puntuacion >= 10) {
+			controlJuego.DesbloquearNivel ();
+		}
+
+	}
+
+	void ActualizarTextoPuntuacion(){
+		textoPuntuacion.text = "Puntos: " + puntuacion.ToString ();
+	}
+
+}
